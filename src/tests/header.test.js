66eca0d117d306a778477c3a93667a72a502e0b8
binhus <eng.robsonwilli@gmail.com>
2020-08-04 17:38:42 -0300

    Vai Ronan

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -2,11 +2,32 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { fireEvent } from '@testing-library/dom';
 import { Provider } from 'react-redux';
-import 'mutationobserver-shim';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import testData from './testData';
-import App from './App';
-import reducer from './reducers';
+import testData from '../../cypress/mocks/areas';
+import App from '../App';
+import reducer from '';
 
 
+console.log(testData);
+const getStore = (initialState) => {
+  if (!initialState) return createStore(reducer, applyMiddleware(thunk));
+  return createStore(reducer, initialState, applyMiddleware(thunk));
+};
+
+const renderApp = (initialState) => {
+  const store = getStore(initialState);
+
+  return {
+    ...render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    ),
+    store,
+  };
+};
+
+test('', () => {
+  expect(testData).toBe(0)
+})
