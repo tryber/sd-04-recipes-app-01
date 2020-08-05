ce1818f2995fbee2d6398fe18a465051f4ca2982
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-08-05 18:52:25 -0300

    new helper

--- src/Components/CardsFood.js
+++ src/Components/CardsFood.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
+import { createNewArr } from '../helpers';
 
 const CardsFood = ({ meals, request }) => {
-  let newArrFoods = [];
 
   if (meals === null) {
     alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
@@ -14,15 +14,9 @@ const CardsFood = ({ meals, request }) => {
 
   if (meals.length === 1 && meals[0].idMeal !== '52968') return <Redirect to={`/comidas/${meals[0].idMeal}`} />;
 
-  if (meals.length > 12) {
-    for (let index = 0; index < 12; index += 1) {
-      newArrFoods.push(meals[index]);
-    }
-  } else newArrFoods = [...meals];
-
   return (
     <div>
-      {newArrFoods.map((item, i) => (
+      {createNewArr(meals).map((item, i) => (
         <Link to={`/comidas/${item.idMeal}`} key={item.idMeal}>
           <div key={item.idMeal} data-testid={`${i}-recipe-card`}>
             <img
