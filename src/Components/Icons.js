a5f627fbd574b9d44a14cd3b2abbb767c71c4cec
binhus <eng.robsonwilli@gmail.com>
2020-08-06 19:14:44 -0300

    adicionando clipboard

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeart from '../images/whiteHeartIcon.svg';
 import blackHeart from '../images/blackHeartIcon.svg';
 import save from '../helpers/index';
 import { getLocalStorage } from '../helpers/index';
+const copy = require('clipboard-copy');
 
 const InputHeart = (heart, callback) => {
   if (callback) {
@@ -21,62 +21,82 @@ const InputHeart = (heart, callback) => {
   return <input type="image" src={heart} data-testid="favorite-btn" />;
 };
 
-const InputShare = (strSource, callback) => (
-  <div>
-    <CopyToClipboard text={`http://localhost:3000${strSource}`}>
+const InputShare = (strSource, callback) => {
+  const handleShare = () => {
+    callback(true);
+    copy(`http://localhost:3000${strSource}`)
+  }
+  return (
+    <div>
       <input
         type="image"
         src={shareIcon}
         data-testid="share-btn"
-        onClick={() => callback(true)}
+        onClick={() => handleShare()}
       />
-    </CopyToClipboard>
-  </div>
-);
+    </div>
+  );
+};
 
+const Comparisor = (idMeal) =>
+  getLocalStorage('favoriteRecipes').find(({ id }) => id === idMeal);
 
-const Comparisor = (idMeal) => (
-  getLocalStorage('favoriteRecipes').find(({ id }) => id === idMeal)
-);
+const foodData = (strMeal, strMealThumb, strCategory, idMeal, strArea) => ({
+  id: idMeal,
+  type: 'comida',
+  area: strArea,
+  category: strCategory,
+  alcoholicOrNot: '',
+  name: strMeal,
+  image: strMealThumb,
+});
 
-const foodData = (strMeal, strMealThumb, strCategory, idMeal, strArea) => (
-  {
-    id: idMeal,
-    type: 'comida',
-    area: strArea,
-    category: strCategory,
-    alcoholicOrNot: '',
-    name: strMeal,
-    image: strMealThumb,
-  }
-);
-
-const drinkData = (strDrink, strDrinkThumb, strCategory, strAlcoholic, idDrink) => (
-  {
-    id: idDrink,
-    type: 'bebida',
-    area: '',
-    category: strCategory,
-    alcoholicOrNot: strAlcoholic,
-    name: strDrink,
-    image: strDrinkThumb,
-  }
-);
+const drinkData = (
+  strDrink,
+  strDrinkThumb,
+  strCategory,
+  strAlcoholic,
+  idDrink,
+) => ({
+  id: idDrink,
+  type: 'bebida',
+  area: '',
+  category: strCategory,
+  alcoholicOrNot: strAlcoholic,
+  name: strDrink,
+  image: strDrinkThumb,
+});
 
 const IconsFood = (props) => {
   const [copied, setCopied] = useState(false);
   const [render, setRender] = useState(false);
-  const { pathName: { path, url } } = props;
+  const {
+    pathName: { path, url },
+  } = props;
   let actualData = [];
   if (path === '/comidas/:id') {
-    const { detailsRecipe: { strMeal, strMealThumb, strCategory, idMeal, strArea } } = props;
+    const {
+      detailsRecipe: { strMeal, strMealThumb, strCategory, idMeal, strArea },
+    } = props;
     actualData = foodData(strMeal, strMealThumb, strCategory, idMeal, strArea);
   }
   if (path === '/bebidas/:id') {
-    const { detailsDrink: {
-      strDrink, strDrinkThumb, strCategory, strAlcoholic, idDrink,
-    } } = props;
-    actualData = drinkData(strDrink, strDrinkThumb, strCategory, strAlcoholic, idDrink);
+    const {
+      detailsDrink: {
+        strDrink,
+        strDrinkThumb,
+        strCategory,
+        strAlcoholic,
+        idDrink,
+      },
+    } = props;
+    actualData = drinkData(
+      strDrink,
+      strDrinkThumb,
+      strCategory,
+      strAlcoholic,
+      idDrink,
+    );
   }
   const infoFromLocalStorage = getLocalStorage('favoriteRecipes');
   const onFavorite = () => {
