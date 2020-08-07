9d1dfb27bce91a3a225f144bdd0fcc0cf9941f51
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-08-07 18:37:28 -0300

    CC

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -3,28 +3,26 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import { getLocalStorage } from '../helpers';
 
-const Profile = ({ location: { pathname } }) => {
-  const user = JSON.parse(localStorage.getItem('user'));
-  return (
-    <div>
-      <Header pathname={pathname} />
-      <h2 data-testid="profile-email">{user.email}</h2>
-      <Link to="/receitas-feitas">
-        <button data-testid="profile-done-btn">Receitas Feitas</button>
-      </Link>
-      <Link to="/receitas-favoritas">
-        <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      </Link>
-      <Link to="/">
-        <button type="submit" data-testid="profile-logout-btn" onClick={() => localStorage.clear()}>
-          Sair
-        </button>
-      </Link>
-      <Footer />
-    </div>
-  );
-};
+const Profile = ({ location: { pathname } }) => (
+  <div>
+    <Header pathname={pathname} />
+    <h2 data-testid="profile-email">{getLocalStorage('user').email}</h2>
+    <Link to="/receitas-feitas">
+      <button data-testid="profile-done-btn">Receitas Feitas</button>
+    </Link>
+    <Link to="/receitas-favoritas">
+      <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
+    </Link>
+    <Link to="/">
+      <button type="submit" data-testid="profile-logout-btn" onClick={() => localStorage.clear()}>
+        Sair
+      </button>
+    </Link>
+    <Footer />
+  </div>
+);
 
 Profile.propTypes = {
   location: PropTypes.shape(PropTypes.string.isRequired).isRequired,
